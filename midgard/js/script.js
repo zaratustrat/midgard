74b2a6001a662c883958aa715b4b39c4fdf9bf27
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-04-18 18:34:10 +0300

    slider

---
+++ midgard/js/script.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function () {
+ $('.slider-one').slick({
+  infinite: true,
+  nextArrow: '<div class="arrow arrow_right"></div>',
+  prevArrow: '<div class="arrow arrow_left"></div>',
+  responsive: [
+   {
+    breakpoint: 1200,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+    }
+   },
+   {
+    breakpoint: 1024,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   },
+   {
+    breakpoint: 768,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   }
+  ]
+ }); 
+});
+
+$(document).ready(function () {
+ $('.slider-two').slick({
+  infinite: true,
+  nextArrow: '<div class="arrow arrow_right"></div>',
+  prevArrow: '<div class="arrow arrow_left"></div>',
+  responsive: [
+   {
+    breakpoint: 1200,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+    }
+   },
+   {
+    breakpoint: 1024,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   },
+   {
+    breakpoint: 768,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   }
+  ]
+ });
+});
+
+$(document).ready(function () {
+ $('.slider-three').slick({
+  infinite: true,
+  nextArrow: '<div class="arrow arrow_right"></div>',
+  prevArrow: '<div class="arrow arrow_left"></div>',
+  responsive: [
+   {
+    breakpoint: 1200,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+    }
+   },
+   {
+    breakpoint: 1024,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   },
+   {
+    breakpoint: 768,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1
+    }
+   }
+  ]
+ });
+});
+
+
